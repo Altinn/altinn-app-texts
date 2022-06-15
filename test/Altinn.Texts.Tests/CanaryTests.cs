17466aa07674c345cff7ec5739f49c315b1842b0
Ronny Birkeli <ronny.birkeli@digdir.no>
2022-06-15 09:49:52 +0200

    chore: solution and project structure

---
+++ test/Altinn.Texts.Tests/CanaryTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Altinn.Texts.Tests
+{
+    public class CanaryTests
+    {
+        [Fact]
+        public void IsAlive()
+        {
+            Assert.True(true);
+        }
+    }
+}
